c16b7660cf708c090ee3a03e38c83eb5188562aa
isungurov <isungurov@2c54a935-e501-0410-bc05-97a93f6bca70>
2010-12-04 14:45:24 +0000

    [JGIT-7] added ${parent} tag which referrence to parent commit hash (to make links to deleted files) added ${blob} tag which referrence to object hash (need for gitweb) fixed default gitweb links

--- src/main/java/com/xiplink/jira/git/linkrenderer/LinkFormatRenderer.java
+++ src/main/java/com/xiplink/jira/git/linkrenderer/LinkFormatRenderer.java
@@ -1,5 +1,6 @@
 package com.xiplink.jira.git.linkrenderer;
 
+import com.atlassian.core.util.map.EasyMap;
 import org.apache.log4j.Logger;
 import org.eclipse.jgit.revwalk.RevCommit;
 
@@ -9,6 +10,8 @@ import com.xiplink.jira.git.GitConstants;
 import com.xiplink.jira.git.GitManager;
 import com.xiplink.jira.git.ViewLinkFormat;
 
+import java.util.Map;
+
 /**
  * A link renderer implementation which lets the user specify the format in the properties file, to accommodate various
  * formats (ViewCVS, Fisheye, etc) out there.
@@ -17,115 +20,113 @@ import com.xiplink.jira.git.ViewLinkFormat;
  * @author Jeff Turner
  */
 public class LinkFormatRenderer implements GitLinkRenderer {
-	private static Logger log = Logger.getLogger(LinkFormatRenderer.class);
-	private String pathLinkFormat;
-	private String fileReplacedFormat;
-	private String fileAddedFormat;
-	private String fileModifiedFormat;
-	private String fileDeletedFormat;
-	private String changesetFormat;
 
-	public LinkFormatRenderer(GitManager gitManager) {
+    private static Logger log = Logger.getLogger(LinkFormatRenderer.class);
+    private String pathLinkFormat;
+    private String fileReplacedFormat;
+    private String fileAddedFormat;
+    private String fileModifiedFormat;
+    private String fileDeletedFormat;
+    private String changesetFormat;
 
-		ViewLinkFormat linkFormat = gitManager.getViewLinkFormat();
-		if (linkFormat != null) {
-			if (linkFormat.getChangesetFormat() != null && linkFormat.getChangesetFormat().trim().length() != 0) {
-				changesetFormat = linkFormat.getChangesetFormat();
-			}
+    public LinkFormatRenderer(GitManager gitManager) {
 
-			if (linkFormat.getFileAddedFormat() != null && linkFormat.getFileAddedFormat().trim().length() != 0) {
-				fileAddedFormat = linkFormat.getFileAddedFormat();
-			}
+        ViewLinkFormat linkFormat = gitManager.getViewLinkFormat();
+        if (linkFormat != null) {
+            if (linkFormat.getChangesetFormat() != null && linkFormat.getChangesetFormat().trim().length() != 0) {
+                changesetFormat = linkFormat.getChangesetFormat();
+            }
 
-			if (linkFormat.getFileModifiedFormat() != null && linkFormat.getFileModifiedFormat().trim().length() != 0) {
-				fileModifiedFormat = linkFormat.getFileModifiedFormat();
-			}
+            if (linkFormat.getFileAddedFormat() != null && linkFormat.getFileAddedFormat().trim().length() != 0) {
+                fileAddedFormat = linkFormat.getFileAddedFormat();
+            }
 
-			if (linkFormat.getFileReplacedFormat() != null && linkFormat.getFileReplacedFormat().trim().length() != 0) {
-				fileReplacedFormat = linkFormat.getFileReplacedFormat();
-			}
+            if (linkFormat.getFileModifiedFormat() != null && linkFormat.getFileModifiedFormat().trim().length() != 0) {
+                fileModifiedFormat = linkFormat.getFileModifiedFormat();
+            }
 
-			if (linkFormat.getFileDeletedFormat() != null && linkFormat.getFileDeletedFormat().trim().length() != 0) {
-				fileDeletedFormat = linkFormat.getFileDeletedFormat();
-			}
+            if (linkFormat.getFileReplacedFormat() != null && linkFormat.getFileReplacedFormat().trim().length() != 0) {
+                fileReplacedFormat = linkFormat.getFileReplacedFormat();
+            }
 
-			if (linkFormat.getViewFormat() != null && linkFormat.getViewFormat().trim().length() != 0) {
-				pathLinkFormat = linkFormat.getViewFormat();
-			}
+            if (linkFormat.getFileDeletedFormat() != null && linkFormat.getFileDeletedFormat().trim().length() != 0) {
+                fileDeletedFormat = linkFormat.getFileDeletedFormat();
+            }
 
-		} else {
-			log.warn("viewLinkFormat is null");
-		}
-	}
+            if (linkFormat.getViewFormat() != null && linkFormat.getViewFormat().trim().length() != 0) {
+                pathLinkFormat = linkFormat.getViewFormat();
+            }
 
-	// TODO
-	public String getCopySrcLink(RevCommit revision, FileDiff logEntryPath) {
-		String revisionNumber = revision.getId().name();
-		return linkPath(pathLinkFormat, logEntryPath.path
-		// .getCopyPath()
-				, revisionNumber
-				// getPathLink(logEntryPath.getCopyPath()
-				, "");
-	}
+        } else {
+            log.warn("viewLinkFormat is null");
+        }
+    }
 
-	public String getRevisionLink(RevCommit revision) {
-		// TODO
-		return getRevisionLink(revision.getId().getName());
-	}
+    // TODO
+    public String getCopySrcLink(RevCommit revision, FileDiff path) {
+        return formatLink(pathLinkFormat, path.path, EasyMap.build(
+                "${rev}", revision.getId().name(),
+                "${path}", path.path));
+    }
 
-	public String getChangePathLink(RevCommit revision, FileDiff path) {
-		String changeType = path.change;
+    public String getRevisionLink(RevCommit revision) {
+        // TODO
+        return getRevisionLink(revision.getId().getName());
+    }
 
-		String revisionNumber = revision.getId().name();
+    public String getChangePathLink(RevCommit revision, FileDiff path) {
+        String changeType = path.change;
 
-		if (GitConstants.MODIFICATION.equals(changeType)) {
-			return linkPath(fileModifiedFormat, path.path, revisionNumber, "");
-		} else if (GitConstants.ADDED.equals(changeType)) {
-			// TODO validate blob
-			String baseId = "";
-			if (path.blobs.length > 1) {
-				baseId = path.blobs[1].name();
-			}
-			return linkPath(fileAddedFormat, path.path, baseId, revisionNumber);
-		} else if (GitConstants.REPLACED.equals(changeType)) {
-			return linkPath(fileReplacedFormat, path.path, revisionNumber, "");
-		} else if (GitConstants.DELETED.equals(changeType)) {
-			return linkPath(fileDeletedFormat, path.path, revisionNumber, "");
-		} else {
-			return linkPath(fileReplacedFormat, path.path, revisionNumber, "");
-		}
-	}
+        Map<String, String> subst = EasyMap.build(
+                "${rev}", revision.getId().name(),
+                "${path}", path.path,
+                "${parent}", revision.getParent(0).getId().name()
+        );
 
-	protected String getRevisionLink(String revisionNumber) {
-		if (changesetFormat != null) {
-			try {
-				String href = StringUtils.replaceAll(changesetFormat, "${rev}", "" + revisionNumber);
-				String shortRevNumber = revisionNumber.substring(0,7);
-				return "<a href=\"" + href + "\">" + shortRevNumber + "...</a>";
-			} catch (Exception ex) {
-				log.error("format error: " + ex.getMessage(), ex);
-			}
-		}
-		return "" + revisionNumber;
+        String format;
+        if (GitConstants.MODIFICATION.equals(changeType)) {
+            format = fileModifiedFormat;
+        } else if (GitConstants.ADDED.equals(changeType)) {
+            subst.put("${blob}", path.blobs[1].name());
+            format = fileAddedFormat;
+        } else if (GitConstants.REPLACED.equals(changeType)) {
+            format = fileReplacedFormat;
+        } else if (GitConstants.DELETED.equals(changeType)) {
+            subst.put("${blob}", path.blobs[0].name());
+            format = fileDeletedFormat;
+        } else {
+            format = fileModifiedFormat;
+        }
 
-	}
+        return formatLink(format, path.path, subst);
+    }
 
-	private String linkPath(final String format, String path, String revisionNumber, String base) {
-		if (format != null) {
+    protected String getRevisionLink(String revisionNumber) {
+        if (changesetFormat != null) {
+            try {
+                String href = StringUtils.replaceAll(changesetFormat, "${rev}", "" + revisionNumber);
+                String shortRevNumber = revisionNumber.substring(0, 7);
+                return "<a href=\"" + href + "\">" + shortRevNumber + "...</a>";
+            } catch (Exception ex) {
+                log.error("format error: " + ex.getMessage(), ex);
+            }
+        }
+        return "" + revisionNumber;
 
-			try {
-				String href = format;
-				if (path != null) {
-					href = StringUtils.replaceAll(href, "${path}", path);
-				}
-				href = StringUtils.replaceAll(href, "${rev}", revisionNumber);
-				href = StringUtils.replaceAll(href, "${base}", base);
+    }
 
-				return "<a href=\"" + href + "\">" + path + "</a>";
-			} catch (Exception ex) {
-				log.error("format error: " + ex.getMessage(), ex);
-			}
-		}
-		return path;
-	}
+    private String formatLink(String format, String path, Map<String, String> substitutions) {
+
+        if (format != null) {
+            String href = format;
+
+            for (Map.Entry<String, String> subst : substitutions.entrySet()) {
+                href = StringUtils.replaceAll(href, subst.getKey(), subst.getValue());
+            }
+
+            return String.format("<a href=\"%s\">%s</a>", href, path);
+        }
+
+        return path;
+    }
 }
