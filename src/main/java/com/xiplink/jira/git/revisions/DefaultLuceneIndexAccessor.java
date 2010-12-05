5daadd1bc94bcbfbe16bb60f5c1181561a57a584
isungurov <isungurov@2c54a935-e501-0410-bc05-97a93f6bca70>
2010-12-05 13:16:35 +0000

    updated to Jira 4.2 libs

--- src/main/java/com/xiplink/jira/git/revisions/DefaultLuceneIndexAccessor.java
+++ src/main/java/com/xiplink/jira/git/revisions/DefaultLuceneIndexAccessor.java
@@ -1,5 +1,6 @@
 package com.xiplink.jira.git.revisions;
 
+import com.atlassian.jira.InfrastructureException;
 import com.atlassian.jira.issue.index.IndexException;
 import com.atlassian.jira.util.LuceneUtils;
 import org.apache.lucene.analysis.Analyzer;
@@ -11,16 +12,21 @@ import org.apache.lucene.index.IndexWriter;
  *
  * @since 0.9.12
  */
-class DefaultLuceneIndexAccessor implements LuceneIndexAccessor
-{
+class DefaultLuceneIndexAccessor implements LuceneIndexAccessor {
 
-    public IndexReader getIndexReader(String path) throws IndexException
-    {
-        return LuceneUtils.getIndexReader(path);
+    public IndexReader getIndexReader(String path) {
+        try {
+            return LuceneUtils.getIndexReader(path);
+        } catch (IndexException e) {
+            throw new InfrastructureException(e);
+        }
     }
 
-    public IndexWriter getIndexWriter(String path, boolean create, Analyzer analyzer) throws IndexException
-    {
-        return LuceneUtils.getIndexWriter(path, create, analyzer);
+    public IndexWriter getIndexWriter(String path, boolean create, Analyzer analyzer) {
+        try {
+            return LuceneUtils.getIndexWriter(path, create, analyzer);
+        } catch (IndexException e) {
+            throw new InfrastructureException(e);
+        }
     }
 }
